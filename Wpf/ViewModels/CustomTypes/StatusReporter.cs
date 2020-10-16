579aeb7ffd59cbceddd4e5fd57aac7c33b56538a
Sergey <s.o.komarov@gmail.com>
2020-10-16 06:14:01 +0300

    Refactoring the StatusReporter class.

--- Wpf/ViewModels/CustomTypes/StatusReporter.cs
+++ Wpf/ViewModels/CustomTypes/StatusReporter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using Core.Interfaces;
 
@@ -6,31 +7,32 @@ namespace Wpf.ViewModels.CustomTypes
 {
     public class StatusReporter : NotifyPropertyChanged, IReporter
     {
-        public string Error { get; private set; }
+        private readonly TextWriter _errorWriter;
+        private readonly TextWriter _infoWriter;
+
+        public StatusReporter()
+        {
+            _errorWriter = Console.Error;
+            _infoWriter = Console.Out;
+        }
 
         public string Status { get; private set; }
 
-        public string Info { get; private set; }
-
-        public void ReportError(string error)
+        public void ReportError(string newError)
         {
-            Error = error;
-            OnPropertyChanged(nameof(Error));
-
-            Console.Error.WriteLine("!!! Error !!! " + error);
+            _errorWriter.WriteLine("!!! Error !!! " + newError);
         }
 
-        public void ReportInfo(string info)
+        public void ReportInfo(string newInfo)
         {
-            Info = info;
-            OnPropertyChanged(nameof(Info));
-
-            Console.Out.WriteLine("Info: " + info);
+            _infoWriter.WriteLine("Info: " + newInfo);
         }
 
-        public void ReportStatus(string status)
+        public void ReportStatus(string newStatus)
         {
-            Status = status;
+            if (Status == newStatus) return;
+
+            Status = newStatus;
             OnPropertyChanged(nameof(Status));
         }
     }
